e5be00c17e2fc556464f28d524503d1be9fc8098
Himanshu Kushwah <himansh9489@gmail.com>
2024-02-13 11:45:25 +0530

    event bubbling and capturing and event delegation

---
+++ EventDelegation.js
@@ -0,0 +1,34 @@
+// document.querySelector("#category").addEventListener("click", (e) => {
+//   console.log(e.target);
+//   if (e.target.tagName === "LI") {
+//     window.location.href = "/" + e.target.id;
+//   }
+// });
+
+document.querySelector("#form").addEventListener("keydown", (e) => {
+  console.log(e);
+  if (e.target.dataset.uppercase === "uppercase") {
+    e.target.value = e.target.value.toUpperCase();
+  }
+});
+
+// Event delegation is a JavaScript technique where you
+// attach a single event listener to a parent element,
+// rather than attaching multiple event listeners to
+// individual child elements. This single event listener
+// then handles events that bubble up from the child elements.
+// Event delegation works because most events in the DOM bubble
+// up from the target element to its ancestors.
+
+// -> Improved Performance
+// -> Dynamic Element Handling
+// -> Simplified Code
+// -> Memory Management
+
+// -> Event Targeting needs to be accurately
+// -> Not all events can be bubble up
+// -> Needs addintion conditional logic
+
+// Not bubbling events ->
+// focus, blur, input ,load,unload,play,pause,volumechange
+// mouseenter mouseleave etc
--- PolyfillForBind.js
+++ PolyfillForBind.js
@@ -16,8 +16,8 @@ let printName = printFullName.bind(name1);
 Function.prototype.myBind = function (...args) {
   //this == printFullName
   //this will point to printFullName fn
-  let obj = this;
-  // console.log("this",obj);
+  let fn = this;
+  // console.log("this",fn);
   // to get parems remove the 1st one from args
   let parem = args.slice(1);
   // bind returns a function so
@@ -27,11 +27,20 @@ Function.prototype.myBind = function (...args) {
     // but needs call with binding reference
     // printFullName.call(name1);
     // this.call(name1);
-    obj.apply(args[0], [...parem, ...args2]);
+    fn.apply(args[0], [...parem, ...args2]);
   };
 };
-let printName1 = printFullName.myBind(name1, "Gwalior");
+// do not use call or apply
+Function.prototype.myBind1 = function (context = {}, ...args) {
+  if (typeof this !== "function")
+    throw new Error(this + "cannot be bound as it's not callable");
+  context.fn = this;
+  return function (...args2) {
+    context.fn(...args, ...args2);
+  };
+};
+let printName1 = printFullName.myBind1(name1, "Gwalior");
 printName1("MP", "Bharat");
 
-let printName2 = printFullName.myBind(name1, "Gwalior", "MP");
+let printName2 = printFullName.myBind1(name1, "Gwalior", "MP");
 printName2("Bharat");
